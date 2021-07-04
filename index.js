9ac8805f65c290ced0cf8aa11486f2c6c2382002
Thibaux Swildens <thibaux.swildens@gmail.com>
2021-07-04 12:17:16 +0200

    Changes

--- index.js
+++ index.js
@@ -2,6 +2,4 @@ const server = require('./src/server.js')
 
 const port = process.env.PORT || 3000;
 
-server.listen(3000, () => {
-    console.log('Listening on port ' + port);
-})
+server.listen(port, () => console.log(`Listening on port ${port}`));
