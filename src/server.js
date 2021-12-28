1cc84af8ce4f51cbe27205885c6476e6afbe1809
Thibaux Swildens <thibaux.swildens@gmail.com>
2021-12-28 20:51:27 +0100

    Updated Access-Control-Allow-Origin to api

--- src/server.js
+++ src/server.js
@@ -9,6 +9,11 @@ server.use(express.json());
 
 server.get('/', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
 })
 
 server.use('/api', routes);
